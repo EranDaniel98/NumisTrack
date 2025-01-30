8829f48b6ef53ebfa48152b9a67ce8fa4849b8aa
Eran <erand1998@gmail.com>
2025-01-30 20:47:52 +0200

    Initialized Express server with MongoDB connection

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => res.send("Welcome to the Coin Collection API"));
+
+mongoose
+    .connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Error connecting to MongoDB:', err));
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log('Server running on http://localhost:${PORT}'));
