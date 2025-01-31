163b195e3d95ed89d64570837a24e734152bf7fd
Eran <erand1998@gmail.com>
2025-01-31 22:14:49 +0200

    Initialized Redux store with user and coin reducers

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,3 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import userReducer from './userSlice'
 import coinReducer from './coinSlice'
+
+const store = configureStore({
+    reducer: {
+        user: userReducer,
+        coin: coinReducer,
+    }
+});
+
+export default store;
