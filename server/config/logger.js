eec1006eab0b715442a7754d7c502fedc8daecbf
Eran <erand1998@gmail.com>
2025-02-05 21:04:25 +0200

    Modified logs logic, seperate files for type of log (error, info, etc), file per day, logs will be deleted after 30 days

---
+++ server/config/logger.js
@@ -0,0 +1,45 @@
+const winston = require('winston');
+const DailyRotateFile = require('winston-daily-rotate-file'); 
+require('winston-mongodb');
+require('dotenv').config();
+
+
+const logFormat = winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.printf(({ timestamp, level, message }) => {
+        return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+    })
+);
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: logFormat,
+    transports: [
+        new winston.transports.Console(),
+        new DailyRotateFile({
+            filename: 'logs/error-%DATE%.log',
+            datePattern: 'YYYY-MM-DD',
+            level: 'error',
+            maxSize: '10m',
+            maxFiles: '30d',
+        }),
+
+        new DailyRotateFile({
+            filename: 'logs/combined-%DATE%.log',
+            datePattern: 'YYYY-MM-DD',
+            maxSize: '10m',
+            maxFiles: '30d',
+        }),
+    ]
+});
+
+if (process.env.MONGO_URI) {
+    logger.add(new winston.transports.MongoDB({
+        db: process.env.MONGO_URI,
+        collection: 'logs',
+        level: 'error',
+        expireAfterSeconds: 60 * 60 * 24 * 30, // 30 days
+    }));
+}
+
+module.exports = logger;
